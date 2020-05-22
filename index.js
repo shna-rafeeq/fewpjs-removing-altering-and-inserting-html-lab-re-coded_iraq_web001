da28f7e227aa55b9d2e07bd8d09866413e92053b
shna rafeeq <shnar.majeed@gmail.com>
2020-05-22 18:38:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Write your code here!
 
+<<<<<<< HEAD
 
 let newHeader = document.querySelector("h1#victory");
 
 
+=======
+document.querySelector('main');
+let newHeader = document.querySelector("h1#victory");
+element.innerHTML = 'is the champion';
+>>>>>>> 81dd6835f4b3a438fa9c8dd84324a996299ad59f
